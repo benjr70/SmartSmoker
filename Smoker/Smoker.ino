ea604c5a27fa926623c444f22c7c0362be7c12a5
benjr70 <benrolf70@gmail.com>
2019-05-12 19:36:49 -0500

    updated arduino temps should be working

--- Smoker/Smoker.ino
+++ Smoker/Smoker.ino
@@ -3,7 +3,8 @@ int analogPin2 = A1;
 //int analogPin3 = 0;
 void setup() {
   
-Serial.begin(115200);
+  //analogReference(INTERNAL);
+  Serial.begin(115200);
     
 }
 
@@ -16,12 +17,18 @@ float vout1 = (read1 * (5.00/ 1023.0));
 int read2 = analogRead(analogPin2);
 float vout2 = (read2 * (5.00/ 1023.0));
 
+
 double Temperature1 = (vout1 - 1.247) / 0.005;
-Serial.print((Temperature1* 9.0/5.0)+32);
-Serial.print(',');
+Temperature1 -= 20;
+Temperature1 = (Temperature1 * 9.0/5.0)+32;
+Serial.print(Temperature1);
+Serial.print(", ");
+
 
 double Temperature2 = (vout2 - 1.247) / 0.005;
-Serial.print((Temperature2* 9.0/5.0)+32);
+Temperature2 -= 20;
+Temperature2 = (Temperature2 * 9.0/5.0)+32;
+Serial.print(Temperature2);
 Serial.println("");
 
 delay(500);
